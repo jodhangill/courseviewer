4b9466b7f1a19b984b1ebfe5f14b379e25484482
Jodhan Gill <jga145@sfu.ca>
2022-12-22 20:50:30 -0800

    Added prerequisites to undergraduate courses

--- courses/courses.js
+++ courses/courses.js
@@ -4,8 +4,7 @@ const fs = require('fs');
 fs.writeFileSync('courses.json', '');
 fs.appendFileSync('courses.json', '[')
 
-
-fetch('http://www.sfu.ca/bin/wcm/course-outlines?2023/summer')
+fetch('http://www.sfu.ca/bin/wcm/course-outlines?2023/spring')
     .then((res) => res.json())
     .then(function(data) {
         for (d in data) {
@@ -14,12 +13,13 @@ fetch('http://www.sfu.ca/bin/wcm/course-outlines?2023/summer')
     })    
 
 function getCourses(department, depsLength) {
-    fetch('http://www.sfu.ca/bin/wcm/course-outlines?2023/summer/' + department)
+    fetch('http://www.sfu.ca/bin/wcm/course-outlines?2023/spring/' + department)
         .then((response) => response.json())
-        .then(function(data) {
-            for (course in data) {
+        .then(async function(data) {
+            for (course in data) {                   
                 let str = data[course].text;
                 data[course].text = department + str;
+                data[course].prereqs = await getPreReqs(str, department);                        
             }
             fs.appendFileSync('courses.json', JSON.stringify(data, null, 4).replace(/[\[\]']+/g,''));
             if (currentNumber == depsLength) {
@@ -31,4 +31,28 @@ function getCourses(department, depsLength) {
             currentNumber++;
 
         });
+}
+
+async function getPreReqs(courseNumber, department) {
+    if (courseNumber < 500) {
+        return fetch('http://www.sfu.ca/bin/wcm/course-outlines?2023/spring/' + department + '/' + courseNumber)
+            .then((response) => response.json())
+            .then(async function(data) {
+                if (data[0]) {
+                    url = 'http://www.sfu.ca/bin/wcm/course-outlines?2023/spring/' + department + '/' + courseNumber + '/' + data[0].text;
+                    pre = await getPreReqs2(url);
+                    return pre;
+                }
+
+            });        
+    }
+}
+
+async function getPreReqs2(url) {
+    return fetch(url)
+        .then((response) => response.json())
+        .then(function(data) {
+            pre = data.info.prerequisites
+            return pre;
+        })
 }
