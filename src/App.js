17449b6af0870aa7e20baadd65ed0d226c22a982
Jodhan Gill <jga145@sfu.ca>
2023-01-08 21:07:12 -0800

    Extracted course selection to increase performance

--- src/App.js
+++ src/App.js
@@ -1,58 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 import {CourseBox} from './course_box/CourseBox'
 import {
   Routes,
   Route,
   Link,
-  useNavigate
 } from 'react-router-dom';
 import React, { useState } from "react";
-import Select from "react-select";
-
-
-
+import CourseList from './CourseSelect';
 
 function App() {
   let courses = require('./course_data/courses.json');
-  let navigate = useNavigate();
-  const [selectedOptions, setSelectedOptions] = useState();
-  const options = [];
-  for (let i = 0; i < courses.length; i++) {
-    let name = courses[i].text;
-
-    let title = name + ": " + courses[i].title;
-    options.push({value: name, label: title})
-
-  } 
-
-  function handleSelect(data) {
-    setSelectedOptions(data);
-    let course = data.value;
-    navigate('/courses/' + course);
-
-  }
 
   return (
     <div className="App">
       <h2>APP</h2>  
-
+      <CourseList />
       {courses.map(course => (<Link to={'/courses/' + course?.text}/>))}        
       <Routes>
         <Route path='/courses/:text' element={<CourseBox/>}></Route>;
-      </Routes>        
-
-
-      <Select
-          options={options}
-          placeholder="Select Course"
-          value={selectedOptions}
-          onChange={handleSelect}
-          isSearchable={true}
-          // isMulti
-        />  
-
-      
+      </Routes>         
     </div>
   );
 }
---
+++ src/CourseSelect.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import Select from "react-select";
+import { FixedSizeList as List } from "react-window";
+
+let courses = require('./course_data/courses.json');
+const options = [];
+for (let i = 0; i < courses.length; i++) {
+  let name = courses[i].text;
+  let title = name + ": " + courses[i].title;
+  options.push({value: name, label: title})
+} 
+
+const height = 35;
+
+function MenuList (props) {
+  const { options, children, maxHeight, getValue } = props;
+  const [value] = getValue();
+  const initialOffset = options.indexOf(value) * height;
+  
+  return (
+    <List
+      height={maxHeight}
+      itemCount={children.length}
+      itemSize={height}
+      initialScrollOffset={initialOffset}
+    >
+      {({ index, style }) => <div style={style}>{children[index]}</div>}
+    </List>
+  );
+}
+
+function CourseList() {
+    let navigate = useNavigate();
+    const [selectedOptions, setSelectedOptions] = useState();
+    function handleSelect(data) {
+      setSelectedOptions(data);
+      let course = data.value;
+      navigate('/courses/' + course);
+    
+    }
+    return (
+        <Select components={{ MenuList }} options={options} onChange={handleSelect} placeholder={'Select Course...'}/>
+    );
+}
+
+export default CourseList;
