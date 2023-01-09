ade2913ce926c3984c78e07ddf688f2feb6a789d
Jodhan Gill <jga145@sfu.ca>
2023-01-08 17:08:09 -0800

    Removed spaces from courses in prereqs. Plan to convert to links next

--- src/course_box/PreReq.js
+++ src/course_box/PreReq.js
@@ -1,11 +1,52 @@
+import { func } from "prop-types";
 import React from "react";
 import { useParams } from "react-router-dom";
 
+const data = require('../course_data/courses.json');
+const deps = require('../course_data/departments.json');
+let positions = []
+
 export function PreReq() {
     const {text} = useParams();
-    const data = require('../course_data/courses.json');
-    const PreReq = data[data.findIndex(item => item.text === text)].prereqs; 
+    let PreReq = data[data.findIndex(item => item.text === text)].prereqs; 
+    if (PreReq) {
+        PreReq = removeSpaces(PreReq);        
+    }
+    console.log(positions);
+    document.createElement('p');
+
     return(
         <p> {PreReq} </p>
     );
+}
+
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substring(0, index) + replacement + this.substring(index + replacement.length);
+}
+
+function removeSpaces(str) {
+    if (str.length > 1) {
+        let success = false;
+        let position = -1;
+        for (let i in deps) {
+            let dep = deps[i].text + ' ';
+            position = str.search(dep);
+
+            if (position >= 0) {
+                positions.push(position);                
+                position = position + dep.length - 1;
+                str = str.slice(0, position) + str.slice(position + 1)
+                success = true;         
+            }
+        }
+        if (success) {
+            return removeSpaces(str);
+        }
+        else {
+            return str;            
+        }
+    }
+    else {
+        return str;
+    }
 }
