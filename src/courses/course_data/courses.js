71b041f81b32fb064b60fe4b2abaf3349f4e0022
Jodhan Gill <jga145@sfu.ca>
2023-05-09 22:27:14 -0700

    sfu courses fetcher now allows for multiple terms

--- src/courses/course_data/courses.js
+++ src/courses/course_data/courses.js
@@ -3,43 +3,60 @@ var currentNumber = 1;
 const fs = require('fs');
 fs.writeFileSync('courses.json', '');
 fs.appendFileSync('courses.json', '[')
+const map = new Map();
 
-fetch('http://www.sfu.ca/bin/wcm/course-outlines?2023/spring')
-    .then((res) => res.json())
-    .then(function(data) {
-        for (d in data) {
-            getCourses(data[d].text, Object.keys(data).length);
-        }       
-    })    
+addTerm("2022", "fall");
 
-function getCourses(department, depsLength) {
-    fetch('http://www.sfu.ca/bin/wcm/course-outlines?2023/spring/' + department)
+function addTerm (year, term) {
+    fetch('http://www.sfu.ca/bin/wcm/course-outlines?' + year + '/' + term)
+        .then((res) => res.json())
+        .then(function(data) {
+            for (d in data) {
+                getCourses(data[d].text, year, term);
+            }       
+        })    
+}
+
+
+function getCourses(department, year, term) {
+    fetch('http://www.sfu.ca/bin/wcm/course-outlines?' + year + '/' + term + '/' + department)
         .then((response) => response.json())
         .then(async function(data) {
-            for (course in data) {                   
+            let list = [];
+            for (course in data) {
+                
                 let str = data[course].text;
-                data[course].text = department + str;
-                data[course].prereqs = await getPreReqs(str, department);                        
+                let strFull = department + str;
+                if (!map.has(strFull)) {
+                    list.push(
+                        {
+                            text: strFull,
+                            value: str,
+                            title: data[course].title,
+                            prereqs: ""
+                        }
+                    );
+                    map.set(strFull);
+                    list[list.length - 1].prereqs = await getPreReqs(str, department);                       
+                }
             }
-            fs.appendFileSync('courses.json', JSON.stringify(data, null, 4).replace(/[\[\]']+/g,''));
-            if (currentNumber == depsLength) {
-                fs.appendFileSync('courses.json', ']');
+            fs.appendFileSync('courses.json', JSON.stringify(list, null, 4).replace(/[\[\]']+/g,''));
+            if (list.length > 0) {
+                fs.appendFileSync('courses.json', ',');      
             }
-            else {
-                fs.appendFileSync('courses.json', ',');                
-            }
-            currentNumber++;
+                      
+
 
         });
 }
 
-async function getPreReqs(courseNumber, department) {
+async function getPreReqs(courseNumber, year, term, department) {
     if (courseNumber < 500) {
-        return fetch('http://www.sfu.ca/bin/wcm/course-outlines?2023/spring/' + department + '/' + courseNumber)
+        return fetch('http://www.sfu.ca/bin/wcm/course-outlines?' + year + '/' + term + '/' + department + '/' + courseNumber)
             .then((response) => response.json())
             .then(async function(data) {
                 if (data[0]) {
-                    url = 'http://www.sfu.ca/bin/wcm/course-outlines?2023/spring/' + department + '/' + courseNumber + '/' + data[0].text;
+                    url = 'http://www.sfu.ca/bin/wcm/course-outlines?' + year + '/' + term + '/' + department + '/' + courseNumber + '/' + data[0].text;
                     pre = await getPreReqs2(url);
                     return pre;
                 }
